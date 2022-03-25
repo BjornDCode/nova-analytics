cfccec3172540715e8cf44a4f5fd1a1ec1f09635
Tamer Ağaoğlu <farerock@gmail.com>
2022-03-25 17:03:22 +0300

    added translate

--- resources/views/navigation.blade.php
+++ resources/views/navigation.blade.php
@@ -1,7 +1,7 @@
 <router-link tag="h3" :to="{name: 'Analytics'}" class="cursor-pointer flex items-center font-normal dim text-white mb-6 text-base no-underline">
     <svg class="sidebar-icon" fill="var(--sidebar-icon)" width="20" height="15" xmlns="http://www.w3.org/2000/svg"><path d="M2.22 10.004H1.11A1.11 1.11 0 0 0 0 11.114v2.219c0 .612.497 1.11 1.11 1.11h1.11a1.11 1.11 0 0 0 1.109-1.11v-2.22a1.11 1.11 0 0 0-1.11-1.11zm5.547-3.33h-1.11a1.11 1.11 0 0 0-1.11 1.11v5.549c0 .612.498 1.11 1.11 1.11h1.11a1.11 1.11 0 0 0 1.11-1.11V7.785a1.11 1.11 0 0 0-1.11-1.11zm5.548-3.328h-1.11a1.11 1.11 0 0 0-1.11 1.11v8.877c0 .612.498 1.11 1.11 1.11h1.11a1.11 1.11 0 0 0 1.11-1.11V4.456a1.11 1.11 0 0 0-1.11-1.11zM18.863.018h-1.11a1.11 1.11 0 0 0-1.109 1.11v12.205c0 .612.497 1.11 1.11 1.11h1.109a1.11 1.11 0 0 0 1.11-1.11V1.128a1.11 1.11 0 0 0-1.11-1.11z"/></svg>
     <span class="sidebar-label">
-        Analytics
+        {{ __('Analytics') }}
     </span>
 </router-link>
 
